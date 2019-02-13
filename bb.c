e72eea062f296fa543cc7d542cf2fd96b1813ae8
adithak <adithak59@gmail.com>
2019-02-13 14:35:19 +0530

    Create bb.c

---
+++ bb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include<conio.h>
+void main() {
+int b[500];
+int i,n,kl;
+printf("enter the elements");
+scanf("%d%d",&n,&kl);
+printf("enter array element");
+for(i=0;i<n;i++)
+scanf("%d",&b[i]);
+for(i=0;i<n;i++)
+{
+	if(k==b[i])
+	{
+		printf("%d",b[i]);
+	}
+}
+
+}
+
+}
